ad23f2997a58bb1e5927e6e5121ed1bba43d900d
Donatas Noreika <donoras@gmail.com>
2023-01-16 08:57:16 +0200

    Kompleksiška paieška veikia

--- main.py
+++ main.py
@@ -35,13 +35,17 @@ while True:
             with conn:
                 c.execute("INSERT INTO automobiliai VALUES (NULL, ?, ?, ?, ?, ?)", (marke, modelis, spalva, metai, kaina))
         case 2:
-            marke = input("Markė: ")
-            modelis = input("Modelis: ")
-            spalva = input("Spalva: ")
+            marke = input("Markė: ") + "%"
+            modelis = input("Modelis: ") + "%"
+            spalva = input("Spalva: ") + "%"
             metai_nuo = input("Metai nuo: ")
+            metai_nuo = metai_nuo if metai_nuo else 1900
             metai_iki = input("Metai iki: ")
+            metai_iki = metai_iki if metai_iki else 2100
             kaina_nuo = input("Kaina nuo: ")
+            kaina_nuo = kaina_nuo if kaina_nuo else 0
             kaina_iki = input("Kaina iki: ")
+            kaina_iki = kaina_iki if kaina_iki else 10000000
             with conn:
                 c.execute(paieska, (marke, modelis, spalva, int(metai_nuo), int(metai_iki), float(kaina_nuo), float(kaina_iki)))
                 print(c.fetchall())
